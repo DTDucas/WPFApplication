bec4a21d749f9024db730dc7e8f151b59afd151f
Duong Tran <105262836+DTDucas@users.noreply.github.com>
2024-11-24 20:39:08 +0700

    Add project files.

---
+++ WPFApplication/MainAppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFApplication
+{
+    public class MainAppConfig : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            MainAppConfig app = new MainAppConfig();
+            var mainWindow = new MainWindow();
+
+            app.Run(mainWindow);
+        }
+    }
+}
